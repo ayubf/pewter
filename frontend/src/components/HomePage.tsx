d61b7b17e906051589b47d97f1554e73ffd333cf
ayubf <razortyphon@gmail.com>
2022-02-16 15:26:28 -0600

    Post rendering

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,7 +1,42 @@
+import {useState, useEffect} from 'react';
+
+
 const HomePage = () => {
+
+    let [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const getPosts = async () => {
+            await fetch("http://localhost:3002/posts/", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(res => res.json())
+            .then(data => setPosts(data.posts))
+        }
+        getPosts()
+    }, [])
+
     return(
         <div>
             <h1>Home Page</h1>
+            {
+                posts.map(({posttitle, userid, posturl, postcontent}) => {
+                    return(
+                        <div className="postDiv" key={posttitle}>
+                            <h4>
+                                <a href={`http://localhost:3002/posts/${posturl}`}>{posttitle}</a>
+                            </h4>
+                            <h5>{userid}</h5>
+                            <p>
+                                    {postcontent}
+                            </p>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
